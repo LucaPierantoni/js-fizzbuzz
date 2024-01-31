a9c846aa983af79e14849fe44a48ce371525a203
LucaPierantoni <luca.pier99@gmail.com>
2024-01-31 18:47:10 +0100

    ciclo con append

--- js/script.js
+++ js/script.js
@@ -2,3 +2,35 @@
 //ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+const container = document.getElementById("container")
+
+for (let i = 1; i <= 100; i++) {
+
+    const box = document.createElement("div");
+
+    //numeri sia multipli di 3 e di 5
+
+    if (i % 15 == 0) {
+        box.classList.add("box", "fizzbuzz");
+        box.innerHTML = "fizzbuzz";
+
+    //numeri multipli di 3
+
+    } else if (i % 3 == 0) {
+        box.classList.add("box", "fizz");
+        box.innerHTML = "fizz";
+
+    //numeri multipli di 5 
+        
+    } else if (i % 5 == 0) {
+        box.classList.add("box", "buzz");
+        box.innerHTML = "buzz";
+    
+    } else {
+        box.classList.add("box");
+        box.innerHTML = i;
+    }
+    
+    container.append(box);
+    
+}
